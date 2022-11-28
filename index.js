4150578c0d1644a674752bdfc423576b5b877411
Arthur Edelstein <arthuredelstein@gmail.com>
2022-11-28 00:17:43 -0800

    evaluate file changes

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+const fs = require("fs");
+const fsPromises = require('node:fs/promises');
 const { parse } = require("@babel/parser");
 const generate = require("@babel/generator").default;
 
 const repl = require('node:repl');
 const options = { useColors: true };
+
+const watchForFileChanges = (path, interval, callback) =>
+  fs.watchFile(
+    path, { interval, persistent: false },
+    async (current, previous) => {
+      if (current.mtime !== previous.mtime) {
+        const contents = await fsPromises.readFile(path, { encoding: 'utf8' });
+        callback(contents);
+      }
+    });
 
 const makeTopLevelDeclarationsMutable = (tree) => {
   const topLevelNodes = tree.program.body;
@@ -22,12 +34,38 @@ const createEvalWithMutableTopLevel = (originalEval) => {
       const tree = parse(code);
       const tree2 = makeTopLevelDeclarationsMutable(tree);
       const generatorResult = generate(tree2, {}, code);
+//      console.log(generatorResult.code);
       await originalEval(generatorResult.code, ...args);
     } catch (e) {
+//      console.log(e);
       await originalEval(code, ...args);
     }
   };
 };
 
+let previousFragments = new Set();
+
+const evaluateChangedCodeFragments = (c) => {
+  //  console.log(c);
+  const tree = parse(c);
+  const currentFragments = new Set();
+  for (let node of tree.program.body) {
+    const fragment = generate(node, {}, "").code;
+    currentFragments.add(fragment);
+    if (previousFragments.has(fragment)) {
+      previousFragments.delete(fragment);
+    } else {
+//      console.log(fragment);
+      newEval(fragment, replServer.context, undefined, () => {});
+    }
+  }
+  for (let fragment of previousFragments) {
+    // TODO: remove old fragment
+  }
+  previousFragments = currentFragments;
+};
+
 const replServer = new repl.REPLServer(options);
-replServer.eval = createEvalWithMutableTopLevel(replServer.eval);
+const newEval = createEvalWithMutableTopLevel(replServer.eval);
+replServer.eval = newEval;
+watchForFileChanges("test.js", 100, evaluateChangedCodeFragments);
