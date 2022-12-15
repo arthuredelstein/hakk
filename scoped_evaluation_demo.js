3d3bfce23875d9fbcc883af563a8fbef2a4f85c3
Arthur Edelstein <arthuredelstein@gmail.com>
2022-12-14 16:00:26 -0800

    scoped_evaluation_demo.js

---
+++ scoped_evaluation_demo.js
@@ -0,0 +1,34 @@
+const scopedEvaluator = function () {
+  // Create a generator that reads a value on yield
+  // evaluates it, and sends the result back.
+  const generator = function* () {
+    let valueToSend = undefined;
+    while (true) {
+      const receivedValue = yield valueToSend;
+      if (receivedValue === ".end") {
+        break;
+      }
+      try {
+        valueToSend = { result: eval(receivedValue)};
+      } catch (e) {
+        valueToSend = { error: e }
+      }
+    }
+  }
+  // Run the generator.
+  let iterator = generator();
+  // Discard first empty value.
+  iterator.next();
+  // Return an evaluation function that
+  // takes code and returns its eval'd value.
+  // If evaluation causes an error, then throw
+  // that error instead.
+  return function (code) {
+    const { result, error } = iterator.next(code).value;
+    if (error) {
+      throw error;
+    } else {
+      return result;
+    }
+  };
+};
