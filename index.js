0c2a48cfb35f8fbe7db49773f062e3ddb2f1890b
Arthur Edelstein <arthuredelstein@gmail.com>
2022-12-21 09:44:30 -0800

    use hex sha256 for filenames

--- index.js
+++ index.js
@@ -30,8 +30,9 @@ const watchForFileChanges = (path, interval, callback) => {
     });
 };
 
+// Take a string and return the sha256 digest in a hex string (64 characters).
 const sha256 = (text) =>
-  createHash('sha256').update(text, 'utf8').digest().toString("base64");
+  createHash('sha256').update(text, 'utf8').digest().toString("hex");
 
 // ## AST transformations
 
