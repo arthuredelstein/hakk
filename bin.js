d6a56c6ea9053ce6e751d31249e6500a1b391471
Arthur Edelstein <arthuredelstein@gmail.com>
2022-12-31 06:48:26 -0800

    flags

--- bin.js
+++ bin.js
@@ -5,4 +5,12 @@ const minimist = require('minimist');
 
 const argv = minimist(process.argv.slice(2));
 const filename = argv._[0];
-lib.run(filename);
+
+const main = () => {
+  lib.run(filename, argv);
+}
+
+if (require.main === module) {
+  main();
+}
+  
