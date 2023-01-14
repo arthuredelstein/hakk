ae7c43b959ee169c557d959f373b54d9048490f9
Arthur Edelstein <arthuredelstein@gmail.com>
2023-01-13 22:51:07 -0800

    update test1.js

--- test1.js
+++ test1.js
@@ -4,6 +4,8 @@ let x = 0;
 
 let y = 10;
 
+console.log(__dirname, __filename);
+
 const bump = () => {
   // console.log"hello there, bump!");
   return x += 1;
@@ -31,68 +33,68 @@ class MyClass {
   myField = 0;
   static myStaticField = 3;
   uninitializedField = 7;
-  static sum(a,b) {
-    return a+b;
+  static sum (a, b) {
+    return a + b;
   }
   #privateField = 11;
-  #privateMethod() { return 13; }
-  reveal() { return this.#privateField + this.#privateMethod(); }
-  setPrivateField(value) { this.#privateField = value; }
-  inc() {
+  #privateMethod () { return 13; }
+  reveal () { return this.#privateField + this.#privateMethod(); }
+  setPrivateField (value) { this.#privateField = value; }
+  inc () {
     this.q_++;
   }
-  dec() {
+  dec () {
     this.q_--;
   }
-  dec2() {
-    this.q_+=-2;
+  dec2 () {
+    this.q_ += -2;
   }
-  dec3() {
-    this.q_+=-3;
+  dec3 () {
+    this.q_ += -3;
   }
-  incN(N) {
-    this.q_+=N;
+  incN (N) {
+    this.q_ += N;
   }
-  inc2() {
-    this.q_+=2;
+  inc2 () {
+    this.q_ += 2;
   }
-  halve() {
+  halve () {
     this.q_ /= 2;
   }
-  async generator() {
+  async generator () {
     return 1;
   }
-  static run() {
+  static run () {
     console.log("run a static method, baby");
   }
-  get q() {
-//    throw new Error("test");
-    return this.q_+"";
+  get q () {
+    //    throw new Error("test");
+    return this.q_ + "";
   }
-  set q(value) {
+  set q (value) {
     this.q_ = value;
     console.log("set to " + value);
   }
-  toString() {
+  toString () {
     return `yabba ${this.q_} dabba`;
   }
 };
 
 class TestClass {
   x_ = 0;
-  inc() {
+  inc () {
     this.x_++;
   }
 };
 
 class ChildClass extends MyClass {
-  check() {
+  check () {
     return "check " + this.q_;
   }
-  halve() {
+  halve () {
     this.q_ %= 2;
   }
-  toString() {
+  toString () {
     return `child`;//+ super.toString();
   }
 }
@@ -105,10 +107,10 @@ const thpbbb = (name, count) => {
   return result.join(" X ");
 };
 
-function toTitleCase(str) {
+function toTitleCase (str) {
   return str.replace(
     /\w\S*/g,
-    function(txt) {
+    function (txt) {
       return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     }
   );
@@ -124,22 +126,22 @@ const deebee = (n) => {
 
 const invertMap = (m) => {
   let result = {};
-  for (let [k,v] of Object.entries(m)) {
+  for (let [k, v] of Object.entries(m)) {
     result[v] = k;
   }
   return result;
-}
+};
 
 const addSome = (a) => 15 + a;
 
 const reverse = (s) => {
   return s.split("").reverse().join("");
-}
+};
 
 const join = (a, b) => {
   return toTitleCase(a) + " " + toTitleCase(b);
 };
 
-if (import.meta === "blah") {
-  console.log("hi");
-}
+//if (import.meta === "blah") {
+//  console.log("hi");
+//}
