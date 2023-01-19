7f0ae9ba1bc26a64bbdaced2e5ba83ba1dc357a0
Arthur Edelstein <arthuredelstein@gmail.com>
2023-01-18 23:12:39 -0800

    instructions

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { Repl } = require('./repl.js');
 const run = async (filename) => {
   const filenameFullPath = path.resolve(filename);
   const moduleManager = await ModuleManager.create(filenameFullPath);
+  console.log('Use shift+up and shift+down to switch between modules.');
   await Repl.start(moduleManager);
 };
 
