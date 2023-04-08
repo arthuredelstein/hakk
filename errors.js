b4e8d84565a3a8a393618c58de5fe445c136f1f5
Arthur Edelstein <arthuredelstein@gmail.com>
2023-04-08 09:09:24 -0700

    comments in errors.js

--- errors.js
+++ errors.js
@@ -1,11 +1,21 @@
+// Map from hash-based code fragment tracker to the latest offset for that
+// code fragment.
 let offsets = {};
 
+// Receive the latest offsets for each known code fragment. Overwrites old
+// offset values for existing code fragments and adds offsets for any new
+// code fragments.
 const updateOffsets = (offsetsMap) => {
   offsets = Object.assign(offsets, offsetsMap);
 }
 
+// A regular expression that allows us to extract the path, tracker hash, lineNo
+// and column from a raw stack line.
 const rawStackLineRegex = /\((.+?)\|([a-f,0-9]+)\:(\d+)\:(\d+)\)/;
 
+// Takes a raw stack trace, rewrites all stack lines with a hash-based tracker
+// to file:line:column with the latest correct source line number, and removes
+// stack lines corresponding to the hakk evaluator.
 const reformatStack = (stack) => {
   const lines = stack.split("\n");
   const newLines = [];
@@ -26,6 +36,7 @@ const reformatStack = (stack) => {
   return newLines.join("\n");
 }
 
+// Enable stack rewriting for all errors.
 const setupStackTraces = () => {
   Error.prepareStackTrace = (err, callsites) => {
     return reformatStack(err.stack);
