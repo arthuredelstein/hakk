1d0fd00f84ba6db0f80a418cf73b0f32537e6ca6
Arthur Edelstein <arthuredelstein@gmail.com>
2023-04-09 11:24:16 -0700

    check for valid filename

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const { ModuleManager } = require('./modules.js');
 const { Repl } = require('./repl.js');
 
 const run = async (filename) => {
+  if (filename === undefined) {
+    console.log("Please specify a valid filename: `hakk my-file.js`");
+    process.exitCode = 1;
+    return;
+  }
   const filenameFullPath = path.resolve(filename);
   const moduleManager = await ModuleManager.create(filenameFullPath);
   await Repl.start(moduleManager);
